9c830aedc01c05950754916f79340c2f86d8dcdc
mrhenrike <mrhenrike@hotmail.com>
2016-04-18 17:51:15 -0300

    Teste de Commit

---
+++ app/src/main/java/br/edu/estaciofcat/smiliapp/MainActivity.java
@@ -0,0 +1,45 @@
+package br.edu.estaciofcat.smiliapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*//Função Voltar para Home
+    public void btn_voltarHome(View view) {
+        setContentView(R.layout.activity_main);
+    }*/
+
+    //Método Entrar Layout Home
+    public void acao_acessarLayoutHome(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    //Método Entrar Layout Pesquisa
+    public  void acao_acessarLayoutBusca(View view){
+        setContentView(R.layout.layout_buscamenu);
+    }
+
+    //Método Buscar Layout Nome
+    public  void acao_BuscarNome(View view){
+        setContentView(R.layout.layout_buscanome);
+    }
+
+    //Método Buscar Layout Matrícula
+    public void acao_BuscarMatr(View view){
+        setContentView(R.layout.layout_buscamatr);
+    }
+
+    //Aqui terá os métodos para busca do banco de dados
+
+    //Screen Help
+    public void screenHelp(View view){
+        setContentView(R.layout.layout_ajuda);
+    }
+}

1bda61b63ddbaeb017dbe9b15a891184ad95a107
mrhenrike <mrhenrike@hotmail.com>
2016-04-19 20:11:03 -0300

    Alterações do Layout (Action Bar) e Strings

--- app/src/main/java/br/edu/estaciofcat/smiliapp/MainActivity.java
+++ app/src/main/java/br/edu/estaciofcat/smiliapp/MainActivity.java
@@ -1,10 +1,11 @@
 package br.edu.estaciofcat.smiliapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
